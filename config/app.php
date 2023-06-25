02d7a427da79f397f3edd6fc7978c738764e98ed
poznerrr <pozner@gmail.com>
2023-06-25 20:02:15 +0300

    feat: add sleepingowlAdmin

--- config/app.php
+++ config/app.php
@@ -41,7 +41,7 @@ return [
     |
     */
 
-    'debug' => (bool) env('APP_DEBUG', false),
+    'debug' => (bool)env('APP_DEBUG', false),
 
     /*
     |--------------------------------------------------------------------------
@@ -185,6 +185,11 @@ return [
         /*
          * Package Service Providers...
          */
+
+        /**
+         * SleepingOwl Service Provider
+         */
+        SleepingOwl\Admin\Providers\SleepingOwlServiceProvider::class,
 
         /*
          * Application Service Providers...
